3033839ce929a4a7778c2e197adbd28aa56a29fb
yaquawa <yaquawa@gmail.com>
2020-07-28 08:00:14 +0900

    fixed the code style

--- src/TypeGuards.ts
+++ src/TypeGuards.ts
@@ -1,21 +1,20 @@
 function isArray<T = any>(obj: any): obj is Array<T> {
-    if (Array.isArray) {
-        return Array.isArray(obj);
-    }
-    return Object.prototype.toString.call(obj) === '[object Array]';
+  if (Array.isArray) {
+    return Array.isArray(obj);
+  }
+  return Object.prototype.toString.call(obj) === '[object Array]';
 }
 
 function isRegexp(obj: any): obj is RegExp {
-    return Object.prototype.toString.call(obj) === '[object RegExp]';
+  return Object.prototype.toString.call(obj) === '[object RegExp]';
 }
 
 function isFunction<T = Function>(obj: any): obj is T {
-    return typeof obj === 'function';
+  return typeof obj === 'function';
 }
 
 function isString(obj: any): obj is string {
-    return typeof obj === 'string';
+  return typeof obj === 'string';
 }
 
-
-export {isArray, isRegexp, isFunction, isString};
+export { isArray, isRegexp, isFunction, isString };
--- tests/EventDispatcher.test.ts
+++ tests/EventDispatcher.test.ts
@@ -1,154 +1,158 @@
-import {EventDispatcher, AnyEvents} from '../src/EventDispatcher';
+import { EventDispatcher, AnyEvents } from '../src/EventDispatcher';
 
 interface Events {
-    click: (...args: any[]) => any
-    touch: (...args: any[]) => any
-    sentResponse(response: string): void
-    getPostTitle(title: string, post: object): void | string
+  click: (...args: any[]) => any;
+  touch: (...args: any[]) => any;
+
+  sentResponse(response: string): void;
+
+  getPostTitle(title: string, post: object): void | string;
 }
 
-const ed = new EventDispatcher<Events>(['click', 'touch', 'sentResponse', 'getPostTitle']);
+const ed = new EventDispatcher<Events>([
+  'click',
+  'touch',
+  'sentResponse',
+  'getPostTitle',
+]);
 
 test('register and trigger event', () => {
-    const argsPassed = ['arg1', 'arg2'];
-    const callback = jest.fn();
+  const argsPassed = ['arg1', 'arg2'];
+  const callback = jest.fn();
 
-    ed.on('click', callback)
-        .on('touch', callback)
-        .one('touch', callback);
+  ed.on('click', callback)
+    .on('touch', callback)
+    .one('touch', callback);
 
+  ed.trigger('click', ...argsPassed);
+  expect(callback).nthCalledWith(1, ...[...argsPassed, { type: 'click' }]);
 
-    ed.trigger('click', ...argsPassed);
-    expect(callback).nthCalledWith(1, ...([...argsPassed, {type: 'click'}]));
+  ed.trigger('touch', ...argsPassed);
+  expect(callback).nthCalledWith(2, ...[...argsPassed, { type: 'touch' }]);
+  expect(callback).nthCalledWith(3, ...[...argsPassed, { type: 'touch' }]);
 
+  ed.trigger(['touch', 'click'], ...argsPassed);
+  expect(callback).nthCalledWith(4, ...[...argsPassed, { type: 'touch' }]);
+  expect(callback).nthCalledWith(5, ...[...argsPassed, { type: 'click' }]);
 
-    ed.trigger('touch', ...argsPassed);
-    expect(callback).nthCalledWith(2, ...([...argsPassed, {type: 'touch'}]));
-    expect(callback).nthCalledWith(3, ...([...argsPassed, {type: 'touch'}]));
-
-
-    ed.trigger(['touch', 'click'], ...argsPassed);
-    expect(callback).nthCalledWith(4, ...([...argsPassed, {type: 'touch'}]));
-    expect(callback).nthCalledWith(5, ...([...argsPassed, {type: 'click'}]));
-
-    expect(callback).toBeCalledTimes(5);
+  expect(callback).toBeCalledTimes(5);
 });
 
-
 test('register event with regular expression', () => {
-    const callback = jest.fn();
-    const ed = new EventDispatcher([/click/]);
+  const callback = jest.fn();
+  const ed = new EventDispatcher([/click/]);
 
-    expect(() => {
-        ed.on('clickFoo', callback);
-    }).not.toThrow();
+  expect(() => {
+    ed.on('clickFoo', callback);
+  }).not.toThrow();
 
-    expect(() => {
-        ed.on('Foo', callback);
-    }).toThrow();
+  expect(() => {
+    ed.on('Foo', callback);
+  }).toThrow();
 });
 
 test('register onetime event', () => {
-    const callback = jest.fn();
+  const callback = jest.fn();
 
-    ed.one('click', callback);
+  ed.one('click', callback);
 
-    ed.trigger('click', []);
-    ed.trigger('click');
+  ed.trigger('click', []);
+  ed.trigger('click');
 
-    expect(callback).toBeCalledTimes(1);
+  expect(callback).toBeCalledTimes(1);
 });
 
 describe('remove the callbacks', () => {
-    test('with callback specified', () => {
-        const callback1 = jest.fn();
-        const callback2 = jest.fn();
+  test('with callback specified', () => {
+    const callback1 = jest.fn();
+    const callback2 = jest.fn();
 
-        ed.on('click', callback1)
-            .on('click', callback2)
-            .off('click', callback1)
-            .trigger('click');
+    ed.on('click', callback1)
+      .on('click', callback2)
+      .off('click', callback1)
+      .trigger('click');
 
-        expect(callback1).not.toBeCalled();
-        expect(callback2).toBeCalled();
-    });
+    expect(callback1).not.toBeCalled();
+    expect(callback2).toBeCalled();
+  });
 
-    test('with callback not specified, removes all the callbacks of the given event type', () => {
-        const callback1 = jest.fn();
-        const callback2 = jest.fn();
+  test('with callback not specified, removes all the callbacks of the given event type', () => {
+    const callback1 = jest.fn();
+    const callback2 = jest.fn();
 
-        ed.on('click', callback1)
-            .on('click', callback2)
-            .off('click')
-            .trigger('click');
+    ed.on('click', callback1)
+      .on('click', callback2)
+      .off('click')
+      .trigger('click');
 
-        expect(callback1).not.toBeCalled();
-        expect(callback2).not.toBeCalled();
-    });
+    expect(callback1).not.toBeCalled();
+    expect(callback2).not.toBeCalled();
+  });
 });
 
 test('throws if the event type is not valid', () => {
-    const callback = jest.fn();
+  const callback = jest.fn();
 
-    expect(() => {
-        ed.on('foo' as 'click', callback);
-    }).toThrow();
+  expect(() => {
+    ed.on('foo' as 'click', callback);
+  }).toThrow();
 
-    expect(() => {
-        ed.one('foo' as 'click', callback);
-    }).toThrow();
+  expect(() => {
+    ed.one('foo' as 'click', callback);
+  }).toThrow();
 
-    expect(() => {
-        ed.trigger('foo' as 'click');
-    }).toThrow();
+  expect(() => {
+    ed.trigger('foo' as 'click');
+  }).toThrow();
 
-    expect(() => {
-        ed.off('foo' as 'click');
-    }).toThrow();
+  expect(() => {
+    ed.off('foo' as 'click');
+  }).toThrow();
 });
 
-
 test('disable or enable the event dispatcher', () => {
-    const callback = jest.fn();
+  const callback = jest.fn();
 
-    ed.on('click', callback)
-        .disable()
-        .trigger('click');
+  ed.on('click', callback)
+    .disable()
+    .trigger('click');
 
-    expect(callback).not.toBeCalled();
+  expect(callback).not.toBeCalled();
 
-    ed.enable().trigger('click');
-    expect(callback).toBeCalled();
+  ed.enable().trigger('click');
+  expect(callback).toBeCalled();
 });
 
 test('set callback context', () => {
-    const context = {}
+  const context = {};
 
-    ed.one('click', function (this: any) {
-        expect(this).toEqual(ed);
-    }).trigger('click');
+  ed.one('click', function(this: any) {
+    expect(this).toEqual(ed);
+  }).trigger('click');
 
-    ed.setCallbackContext(context).one('click', function (this: any) {
-        expect(this).toEqual(context);
-    }).trigger('click');
+  ed.setCallbackContext(context)
+    .one('click', function(this: any) {
+      expect(this).toEqual(context);
+    })
+    .trigger('click');
 });
 
 test('mixin to another class', () => {
-    class BaseClass {}
+  class BaseClass {}
 
-    const response = 'hello world!';
+  const response = 'hello world!';
 
-    for (let Api of [ed.mixin(BaseClass)]) {
-        let MixedClass = class extends Api {
-            listenSentResponse() {
-                this.on('sentResponse', (res: string) => {
-                    expect(res).toEqual(response);
-                });
-            }
-        }
+  for (let Api of [ed.mixin(BaseClass)]) {
+    let MixedClass = class extends Api {
+      listenSentResponse() {
+        this.on('sentResponse', (res: string) => {
+          expect(res).toEqual(response);
+        });
+      }
+    };
 
-        let mixedObj = new MixedClass();
-        mixedObj.listenSentResponse();
-        mixedObj.trigger('sentResponse', response);
-    }
+    let mixedObj = new MixedClass();
+    mixedObj.listenSentResponse();
+    mixedObj.trigger('sentResponse', response);
+  }
 });
